4f7a94b993e7c4a771fa1d4504903d91cbb32333
Lacy Morrow <me@lacymorrow.com>
2024-09-08 05:22:55 -0400

    signature

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
 	return <footer className="">
 		<div className="container">
 			<div className="flex flex-col items-center justify-center">
-				<p>Made with ❤️ by <a href="https://twitter.com/lacybuilds" className={`text-[${COLORS.LACY}] hover:text-[hsl(237,100%,60%)]`}>@lacybuilds</a></p>
+				<p>Made with ❤️ by <a href="https://www.caitrussellphotography.com/" className={`text-[${COLORS.CAIT}] hover:text-[hsl(237,100%,60%)]`}>ℂ𝒶𝒾𝓉</a> and <a href="https://twitter.com/lacybuilds" className={`text-[${COLORS.LACY}] hover:text-[hsl(237,100%,60%)]`}>ℒ𝒶𝒸𝓎</a></p>
 			</div>
 		</div>
 	</footer>;
