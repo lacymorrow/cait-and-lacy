6a4fc536cbc9b2b39133a3fc325d5f1cf55a84b1
Lacy Morrow <me@lacymorrow.com>
2026-03-26 15:15:45 -0400

    feat: add Umami analytics tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
+import Script from "next/script";
 
 import { TRPCReactProvider } from "@/trpc/react";
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en" className={`${GeistSans.variable}`}>
       <body>
         <TRPCReactProvider>{children}</TRPCReactProvider>
+        <Script defer src="https://analytics.lacy.sh/script.js" data-website-id="f7ac9591-b254-4b5d-8f3d-4ea26d1674b1" />
       </body>
     </html>
   );
